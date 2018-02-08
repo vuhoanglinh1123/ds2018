a4f44794a4b1f30e6565dc788785212a88c800a6
DangVinhBao <sieusaoda@gmail.com>
2018-02-08 23:20:59 +0700

    Minor fix of Labwork 5 code

--- Labwork 5/FindLongestPath.java
+++ Labwork 5/FindLongestPath.java
@@ -64,15 +64,20 @@ public class FindLongestPath {
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
+    
+    Job job = Job.getInstance(conf, "Find Longest Path");
     job.setJarByClass(FindLongestPath.class);
+    
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
+    
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
+    
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
